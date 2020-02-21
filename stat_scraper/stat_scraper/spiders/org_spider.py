fc91cb52b0e45b8bf567378e16e64eb09a40380c
Harshit Maurya <hmaurya999@gmail.com>
2020-02-21 11:47:44 +0530

    get all projects since 2016

--- stat_scraper/stat_scraper/spiders/org_spider.py
+++ stat_scraper/stat_scraper/spiders/org_spider.py
@@ -1,27 +1,30 @@
 import scrapy
+from datetime import datetime
 
 
 class OrgSpider(scrapy.Spider):
     name = "orgs"
-    base_url = "https://summerofcode.withgoogle.com"
+    base_url = dict(
+        pre_2016="https://www.google-melange.com",
+        since_2016="https://summerofcode.withgoogle.com",
+    )
 
     def start_requests(self):
-        urls = [
-            "https://summerofcode.withgoogle.com/archive/2018/organizations/"
-        ]
+        this_year = datetime.now().year
+        for year in range(2016, this_year):
+            url = "https://summerofcode.withgoogle.com/archive/{}/organizations/".format(year)
+            yield scrapy.Request(url=url, callback=self.parse_since_2016, meta=dict(year=year))
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
+    def parse_since_2016(self, response):
         org_list_elements = response.css('li.organization-card__container')
+        year = response.meta['year']
         for org_list_element in org_list_elements:
             projects_link__relative = org_list_element.css('a::attr(href)').get()
-            projects_link__absolute = self.base_url + projects_link__relative
-            yield scrapy.Request(projects_link__absolute, callback=self.parse_projects_page)
+            projects_link__absolute = self.base_url['since_2016'] + projects_link__relative
+            yield scrapy.Request(projects_link__absolute, callback=self.parse_projects_page, meta=dict(year=year))
 
     def parse_projects_page(self, response):
+        year = response.meta['year']
         org_name = response.css('h3.banner__title::text').get()
-        print(org_name)
         projects = response.css('ul.project-list-container').css('li')
-        print(len(projects))
+        print(year, org_name, len(projects))
