0dc406f7637ea4092fd9f2728486c2d5384c8ac6
Harshit Maurya <hmaurya999@gmail.com>
2020-02-21 11:33:17 +0530

    remove dump file

--- stat_scraper/stat_scraper/spiders/org_spider.py
+++ stat_scraper/stat_scraper/spiders/org_spider.py
@@ -14,12 +14,11 @@ class OrgSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        with open("dump.html", "w") as f:
-            org_list_elements = response.css('li.organization-card__container')
-            for org_list_element in org_list_elements:
-                projects_link__relative = org_list_element.css('a::attr(href)').get()
-                projects_link__absolute = self.base_url + projects_link__relative
-                yield scrapy.Request(projects_link__absolute, callback=self.parse_projects_page)
+        org_list_elements = response.css('li.organization-card__container')
+        for org_list_element in org_list_elements:
+            projects_link__relative = org_list_element.css('a::attr(href)').get()
+            projects_link__absolute = self.base_url + projects_link__relative
+            yield scrapy.Request(projects_link__absolute, callback=self.parse_projects_page)
 
     def parse_projects_page(self, response):
         org_name = response.css('h3.banner__title::text').get()
