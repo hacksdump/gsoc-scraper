0aa1d3ada1a66e87fa53a80ca00af345ced6c1cb
Harshit Maurya <hmaurya999@gmail.com>
2020-02-21 12:08:50 +0530

    get all projects before 2016

--- stat_scraper/stat_scraper/spiders/org_spider.py
+++ stat_scraper/stat_scraper/spiders/org_spider.py
@@ -12,8 +12,28 @@ class OrgSpider(scrapy.Spider):
     def start_requests(self):
         this_year = datetime.now().year
         for year in range(2016, this_year):
-            url = "https://summerofcode.withgoogle.com/archive/{}/organizations/".format(year)
+            url = "{}/archive/{}/organizations/".format(self.base_url['since_2016'], year)
             yield scrapy.Request(url=url, callback=self.parse_since_2016, meta=dict(year=year))
+
+        for year in range(2009, 2016):
+            url = "{}/archive/gsoc/{}".format(self.base_url['pre_2016'], year)
+            yield scrapy.Request(url=url, callback=self.parse_before_2016, meta=dict(year=year))
+
+    def parse_before_2016(self, response):
+        org_list_elements = response.css('ul.mdl-list').css('li')
+        year = response.meta['year']
+        for org_list_element in org_list_elements:
+            projects_link__relative = org_list_element.css('a::attr(href)').get()
+            projects_link__absolute = self.base_url['pre_2016'] + projects_link__relative
+            yield scrapy.Request(projects_link__absolute,
+                                 callback=self.parse_projects_page__pre_2016,
+                                 meta=dict(year=year))
+
+    def parse_projects_page__pre_2016(self, response):
+        year = response.meta['year']
+        org_name = response.css('h3::text').get()
+        projects = response.css('ul.mdl-list').css('li')
+        print(year, org_name, len(projects))
 
     def parse_since_2016(self, response):
         org_list_elements = response.css('li.organization-card__container')
@@ -21,9 +41,11 @@ class OrgSpider(scrapy.Spider):
         for org_list_element in org_list_elements:
             projects_link__relative = org_list_element.css('a::attr(href)').get()
             projects_link__absolute = self.base_url['since_2016'] + projects_link__relative
-            yield scrapy.Request(projects_link__absolute, callback=self.parse_projects_page, meta=dict(year=year))
+            yield scrapy.Request(projects_link__absolute,
+                                 callback=self.parse_projects_page__since_2016,
+                                 meta=dict(year=year))
 
-    def parse_projects_page(self, response):
+    def parse_projects_page__since_2016(self, response):
         year = response.meta['year']
         org_name = response.css('h3.banner__title::text').get()
         projects = response.css('ul.project-list-container').css('li')
